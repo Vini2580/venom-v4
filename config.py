be77c047d14c8a194c20b79add2740d1b0da23d2
Vini2580 <vini141703@gmail.com>
2024-08-31 22:55:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7447094201:AAFbt-B0MxsbXmJtwXdJ6fDLOM42qRT6uZ8")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Vini_1417")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1460207637")
 
 
 
